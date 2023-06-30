287cad5ac89be97165dd37c286bcb07edb58f3a9
Luka Souza <lukasouza77@gmail.com>
2023-06-30 05:50:57 -0300

    '.'

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+import api from './Api.js';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  useEffect(() => {
+    api.get('/').then(res=> {
+      console.log(res.data);
+    })
+  }, [])
+
   return (
     <div className="App">
 
